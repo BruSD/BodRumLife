12724047d0956430e7011d7645391ac6ffd4fc54
Sofia <sofia.nayda@mobilesoft365.com>
2013-08-28 16:26:55 +0300

    update MA

--- src/com/bodrumlife/mobilesoft365/MyActivity.java
+++ src/com/bodrumlife/mobilesoft365/MyActivity.java
@@ -3,6 +3,8 @@ package com.bodrumlife.mobilesoft365;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.bodrumlife.mobilesoft365.AsyncTask.AsyncTaskGetAllEvent;
+
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
@@ -11,6 +13,11 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
 
     }
