1b59fd673599841af15ca22460b7662ea24612a1
Sofia <sofia.nayda@mobilesoft365.com>
2013-08-29 11:38:35 +0300

    AsyncTask that returned List of HashMaps for four

---
+++ src/com/bodrumlife/mobilesoft365/Asynctask/AsyncTaskFourItems.java
@@ -0,0 +1,117 @@
+package com.bodrumlife.mobilesoft365.AsyncTask;
+
+/**
+ * Created by Sofia on 8/29/13.
+ */
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class AsyncTaskFourItems extends AsyncTask<String, Void, List<HashMap<String, ?>>> {
+
+    private Activity activity;
+    ProgressDialog dialog;
+    String result = "";
+    JSONArray jsonObject = null;
+
+    public AsyncTaskFourItems(Activity activity){
+        this.activity = activity;
+
+    }
+    @Override
+    protected void onPreExecute()
+    {
+        super.onPreExecute();
+        dialog = new ProgressDialog(activity);
+        dialog.setMessage("Loading list");
+        dialog.show();
+    }
+
+    @Override
+    protected List <HashMap<String,?>>  doInBackground(String... params) {
+
+        List <HashMap<String,?>> list1 = new ArrayList<HashMap<String,?>>();
+        URL url = null;
+        JSONArray mArray=null;
+
+
+        try {
+            url = new URL("http://bodrumlife.com/service/index.php?cat="+params[0]);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try{
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                InputStream stream = conn.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(stream));
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while((line =rd.readLine() )!=null)
+                {
+                    builder.append(line);
+                }
+                result=builder.toString();
+                mArray = new JSONArray(result);
+
+                for (int i = 0; i < mArray.length(); i++) {
+                    JSONObject object = mArray.getJSONObject(i);
+                    String id = object.getString("Id");
+                    String name = object.getString("Name");
+                    String details = object.getString("Details");
+                    String image = object.getString("Image");
+
+
+                    HashMap<String,Object> item;
+                    item = new HashMap <String, Object >();
+                    item.put("Id",id);
+                    item.put("Name", name);
+                    item.put("Details",details);
+                    item.put("Image",image);
+                    list1.add(item);
+                }
+            }
+        }
+
+        catch (IOException e) {
+
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            jsonObject=new JSONArray(result);
+
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list1;
+    }
+
+    @Override
+    protected void onPostExecute(List<HashMap<String, ?>> hashMaps) {
+        super.onPostExecute(hashMaps);
+        if(dialog!=null && dialog.isShowing())
+            dialog.dismiss();
+    }
+}
--- src/com/bodrumlife/mobilesoft365/MyActivity.java
+++ src/com/bodrumlife/mobilesoft365/MyActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.bodrumlife.mobilesoft365.AsyncTask.AsyncTaskDetails;
-import com.bodrumlife.mobilesoft365.AsyncTask.AsyncTaskForFourItems;
 import com.bodrumlife.mobilesoft365.AsyncTask.AsyncTaskEnumeration;
 
 
@@ -21,11 +20,6 @@ public class MyActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-//        AsyncTaskForTwoItems asyncTaskForTwoItems = new AsyncTaskForTwoItems(this);
-//        int value=AsyncTaskEnumeration.TypeOfAsyncTask.Hotels.getValue();
-//        asyncTaskForTwoItems.execute(String.valueOf(value));
-        AsyncTaskDetails asyncTaskForFourItems = new AsyncTaskDetails(this);
-        int valeu=AsyncTaskEnumeration.TypeOfAsyncTask.Concerts.getValue();
-        asyncTaskForFourItems.execute("2245",String.valueOf(valeu));
+
     }
 }
